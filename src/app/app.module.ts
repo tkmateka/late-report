a26b1b87a44c34a8522837f519cab0c742bb1925
Tukiso <tkmateka@gmail.com>
2024-01-18 15:29:19 +0200

    Completed the authentication system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { SignInComponent } from './compenents/sign-in/sign-in.component';
 import { SignUpComponent } from './compenents/sign-up/sign-up.component';
 import { PageNotFoundComponent } from './compenents/page-not-found/page-not-found.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './modules/material/material.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,10 @@ import { PageNotFoundComponent } from './compenents/page-not-found/page-not-foun
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/compenents/sign-up/sign-up.component.ts
+++ src/app/compenents/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,5 +8,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent {
+  registerFormData: any = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  }
 
+  constructor(private snackbar: MatSnackBar, private router: Router) { }
+
+  submit(): void {
+    // Check if password was confirmed
+    if(this.registerFormData.password !== this.registerFormData.confirmPassword) {
+      this.snackbar.open(`Password don't match`, 'Ok', { duration: 3000 })
+      return;
+    }
+
+    // Fetch all users
+    let _users = localStorage.getItem('users');
+    const users = _users ? JSON.parse(_users) : []
+
+    if (users?.length > 0) {
+      // Check if user already exist
+      const foundUser = users.find((user: any) => users.email === this.registerFormData.email);
+
+      if (foundUser) {
+        // If it does
+        this.snackbar.open(`${foundUser.email} already exist`, 'Ok', { duration: 3000 })
+      } else {
+        this.addNewUser(users);
+      }
+    } else {
+      this.addNewUser(users);
+    }
+  }
+
+  addNewUser(users:any):void {
+    // Remove confirmPassword prperty
+    delete this.registerFormData.confirmPassword;
+    // If it doesn't, add the new user to the list
+    users.push(this.registerFormData);
+    // Update local storage
+    localStorage.setItem('users', JSON.stringify(users));
+    this.snackbar.open(`${this.registerFormData.email} was added successfully`, 'Ok', { duration: 3000 });
+    // Send user to the login page
+    this.router.navigate(['/sign-in']);
+  }
 }

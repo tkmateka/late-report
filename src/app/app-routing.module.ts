07a243b531d055e6f7ce4f8e9d963a4287f8ef33
Tukiso <tkmateka@gmail.com>
2024-01-18 14:17:24 +0200

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './compenents/page-not-found/page-not-found.component';
+import { SignInComponent } from './compenents/sign-in/sign-in.component';
+import { SignUpComponent } from './compenents/sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/sign-in', pathMatch: 'full' }, // redirect to `first-component`
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './compenents/sign-in/sign-in.component';
+import { SignUpComponent } from './compenents/sign-up/sign-up.component';
+import { PageNotFoundComponent } from './compenents/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    SignUpComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
